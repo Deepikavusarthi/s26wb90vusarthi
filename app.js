a4ca7b190e08587dc42cb45b14cd6d1b6fd31e33
Deepika Vusarthi <123539808+Deepikavusarthi@users.noreply.github.com>
2026-03-31 13:45:07 -0500

    updated

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var zoosRouter = require('./routes/zoos');   // ✅ IMPORTANT
+var zoosRouter = require('./routes/zoos');   
+var gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/zoos', zoosRouter);   // ✅ IMPORTANT
+app.use('/zoos', zoosRouter);
+app.use('/grid', gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

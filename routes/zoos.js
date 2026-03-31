ab605eb88a5126a7e262956acafc09f6c4ab55bc
Deepika Vusarthi <123539808+Deepikavusarthi@users.noreply.github.com>
2026-03-31 13:17:20 -0500

    update

--- routes/zoos.js
+++ routes/zoos.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET zoos page. */
 router.get('/', function(req, res, next) {
   res.render('zoos', { title: 'Search Results Zoos' });
 });
